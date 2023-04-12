e010c875a1c1011006bb46e2b95fea3fa84c2e58
hetongxue303 <1974597686@qq.com>
2023-04-12 21:57:23 +0800

    feat: 测试layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,8 +1,48 @@
 <script setup lang="ts"></script>
 
 <template>
-    layout
-    <router-view />
+    <div class="m-container">
+        <div class="m-header"></div>
+        <div class="m-container">
+            <div class="m-aside"></div>
+            <div class="m-container">
+                <div class="m-main"></div>
+                <div class="m-footer"></div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.m-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: #bfcbd9;
+}
+
+.m-header {
+    height: 60px;
+    position: relative;
+    width: auto;
+    background-color: aqua;
+}
+
+.m-aside {
+    width: 210px;
+    height: 100%;
+    background-color: chartreuse;
+}
+
+.m-main {
+    width: 100%;
+    height: 100%;
+    background-color: darkkhaki;
+}
+
+.m-footer {
+    width: auto;
+    height: 60px;
+    background-color: deepskyblue;
+}
+</style>
